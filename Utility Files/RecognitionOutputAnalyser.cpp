bd93eb5031b7030a02fffb13be0b6d9edbf41e05
thassa2s <teena.ch@gmail.com>
2013-08-04 19:17:33 +0200

    Added age-group-wise statistics computation

--- Utility Files/RecognitionOutputAnalyser.cpp
+++ Utility Files/RecognitionOutputAnalyser.cpp
@@ -12,6 +12,8 @@
 
 using namespace std;
 
+#define FACES 1
+
 unsigned int confusion_matrix_count[ 7 ][ 7 ]; //7 expressions
 double confusion_matrix_percentage[ 7 ][ 7 ]; //7 expressions
 unsigned int expression_count[ 7 ];
@@ -21,11 +23,20 @@ unsigned int total_image_count = 0;
 double expression_wise_recognition_rate[ 7 ];
 double overall_recognition_rate = 0.0;
 
+unsigned int confusion_matrix_count_age_group_wise[ 3 ][ 7 ][ 7 ]; //3 age-groups, max 7 expressions
+double confusion_matrix_percentage_age_group_wise[ 3 ][ 7 ][ 7 ];
+unsigned int expression_count_age_group_wise[ 3 ][ 7 ];
+unsigned int age_group_count[ 3 ];
+unsigned int count_correct_recognition_age_group_wise[ 3 ];
+double age_group_wise_recognition_rate[ 3 ];
+
 const string statistics_output_file_name = "FER_Statistics.txt";
 
 enum expressions { UNKNOWN, ANGER, DISGUST, FEAR, JOY, NEUTRAL, SADNESS, SURPRISE } expr_actual_enum, expr_recognized_enum;
 
-expressions get_enum_const( string expr_label )
+enum age_groups { INVALID, YOUNG, MIDDLE, OLD } age_group_enum;
+
+expressions get_expr_enum_const( string expr_label )
 {
 	if ( strcmp( expr_label.c_str(), "anger" ) == 0 )
 		return ANGER;
@@ -44,24 +55,59 @@ expressions get_enum_const( string expr_label )
 	return UNKNOWN;
 }
 
-string get_enum_string( expressions enum_const )
+string get_expr_enum_string( expressions enum_const )
 {
-	if ( enum_const == ANGER )
-		return string( "anger" );
-	if ( enum_const == DISGUST )
-		return string( "disgust" );
-	if ( enum_const == FEAR )
-		return string( "fear" );
-	if ( enum_const == JOY )
-		return string( "joy" );
-	if ( enum_const == NEUTRAL )
-		return string( "neutral" );
-	if ( enum_const == SADNESS )
-		return string( "sadness" );
-	if ( enum_const == SURPRISE )
-		return string( "surprise" );
-	return string( "unknown" );
+	switch ( enum_const )
+	{
+	case ANGER		: 	return string( "anger" );
+	case DISGUST	: 	return string( "disgust" );
+	case FEAR		:	return string( "fear" );
+	case JOY		: 	return string( "joy" );
+	case NEUTRAL	:	return string( "neutral" );
+	case SADNESS	:	return string( "sadness" );
+	case SURPRISE	:	return string( "surprise" );
+	default			:	return string( "unknown" );
+	}
+}
 
+age_groups get_age_group_enum_const( char age_grp )
+{
+	switch ( age_grp )
+	{
+	case 'y' : return YOUNG;
+	case 'm' : return MIDDLE;
+	case 'o' : return OLD;
+	default  : return INVALID;
+	}
+}
+
+string get_age_group_enum_string( age_groups enum_const )
+{
+	switch ( enum_const )
+	{
+	case YOUNG		: 	return string( "young" );
+	case MIDDLE		: 	return string( "middle-aged" );
+	case OLD		:	return string( "old" );
+	default			:	return string( "invalid" );
+	}
+}
+
+age_groups find_age_group( string image_name )
+{
+	int i = 0;
+	char ch = 'n';
+	boost::char_separator<char> separator("_");
+	boost::tokenizer< boost::char_separator<char> > string_tokens(image_name, separator);
+    for( boost::tokenizer< boost::char_separator<char> >::iterator it = string_tokens.begin(); it != string_tokens.end(); it++ )
+    {
+		i++;
+		if ( i == 2 )
+		{
+			ch = it.current_token().at( 0 );
+			break;
+		}
+	}
+    return get_age_group_enum_const( ch );
 }
 
 void initialise_global_vars()
@@ -69,12 +115,21 @@ void initialise_global_vars()
 	count_correct_recognition = total_image_count = overall_recognition_rate = 0;
     for ( int k = 0; k < 7; k++ )
     {
-    	expression_count[ k ] = expression_percentage[ k ] = expression_wise_recognition_rate[ k ] =0;
+    	expression_count[ k ] = expression_percentage[ k ] = expression_wise_recognition_rate[ k ] = 0;
     	for ( int j = 0; j < 7; j++ )
     		confusion_matrix_count[ k ][ j ] = confusion_matrix_percentage[ k ][ j ] = 0;
     }
+    for ( int k = 0; k < 3; k++ )
+    {
+    	age_group_count[ k ] = count_correct_recognition_age_group_wise[ k ] = age_group_wise_recognition_rate[ k ] = 0;
+    	for ( int j = 0; j< 7; j++ )
+    	{
+    		expression_count_age_group_wise[ k ][ j ] = 0;
+    		for ( int i = 0; i < 7; i++ )
+    			confusion_matrix_count_age_group_wise[ k ][ j ][ i ] = confusion_matrix_percentage_age_group_wise[ k ][ j ][ i ] = 0;
+    	}
+    }
 }
-
 void update_statistics()
 {
 	 for ( int k = 0; k < 7; k++ )
@@ -87,6 +142,22 @@ void update_statistics()
 	    expression_wise_recognition_rate[ k ] = confusion_matrix_count[ k ][ k ] * 100 / ( ( double ) expression_count[ k ] );
 	}
 	overall_recognition_rate = count_correct_recognition * 100 / ( ( double ) total_image_count );
+
+	if ( FACES )
+	{
+		for ( int k = 0; k < 3; k++ )
+		{
+		  	for ( int j = 0; j< 7; j++ )
+		  	{
+		  		for ( int i = 0; i < 7; i++ )
+		  		{
+		  			confusion_matrix_percentage_age_group_wise[ k ][ j ][ i ] = confusion_matrix_count_age_group_wise[ k ][ j ][ i ] * 100/( ( double ) expression_count_age_group_wise[ k ][ j ] );
+		  		}
+		  		count_correct_recognition_age_group_wise[ k ] += confusion_matrix_count_age_group_wise[ k ][ j ][ j ];
+		  	}
+		   	age_group_wise_recognition_rate[ k ] = count_correct_recognition_age_group_wise[ k ] * 100 / ( ( double ) age_group_count[ k ] );
+		}
+	}
 }
 
 void write_statistics_to_file( string folder_path )
@@ -102,7 +173,7 @@ void write_statistics_to_file( string folder_path )
 	outfile << endl << "Confusion_matrix count " << endl << endl;
 	for ( int k = 0; k < 7; k++ )
 	{
-		outfile << get_enum_string( static_cast< expressions >( k + 1 ) ) << '\t';
+		outfile << get_expr_enum_string( static_cast< expressions >( k + 1 ) ) << '\t';
 	  	for ( int j = 0; j < 7; j++ )
 	   	{
 	        outfile << '\t' << confusion_matrix_count[ k ][ j ];
@@ -112,7 +183,7 @@ void write_statistics_to_file( string folder_path )
 	outfile << endl << "Confusion_matrix percentage " << endl << endl;
 	for ( int k = 0; k < 7; k++ )
 	{
-		outfile << get_enum_string( static_cast< expressions >( k + 1 ) ) << '\t';
+		outfile << get_expr_enum_string( static_cast< expressions >( k + 1 ) ) << '\t';
      	for ( int j = 0; j < 7; j++ )
 	   	{
 	        outfile << "\t " << confusion_matrix_percentage[ k ][ j ];
@@ -122,24 +193,77 @@ void write_statistics_to_file( string folder_path )
 	outfile << endl << "Expression count " << endl << endl;
 	for ( int k = 0; k < 7; k++ )
 	{
-		outfile << get_enum_string( static_cast< expressions >( k + 1 ) ) << '\t';
+		outfile << get_expr_enum_string( static_cast< expressions >( k + 1 ) ) << '\t';
 	   	outfile << '\t' << expression_count[ k ] << endl;
 	}
 	outfile << endl << "Expression percentage " << endl << endl;
 	for ( int k = 0; k < 7; k++ )
 	{
-		outfile << get_enum_string( static_cast< expressions >( k + 1 ) ) << '\t';
+		outfile << get_expr_enum_string( static_cast< expressions >( k + 1 ) ) << '\t';
 	 	outfile << '\t' << expression_percentage[ k ] << endl;
 	}
 	outfile << endl << "Expression wise recognition rate " << endl << endl;
 	for ( int k = 0; k < 7; k++ )
 	{
-		outfile << get_enum_string( static_cast< expressions >( k + 1 ) ) << '\t';
+		outfile << get_expr_enum_string( static_cast< expressions >( k + 1 ) ) << '\t';
 	 	outfile << '\t' << expression_wise_recognition_rate[ k ] << endl;
 	}
 	outfile << endl << "Total images: " << total_image_count << endl;
 	outfile << endl << "Success count: " << count_correct_recognition << endl;
 	outfile << endl << "Overall recognition rate: " << overall_recognition_rate << endl;
+
+	if ( FACES )
+	{
+		outfile << endl << "Age-group-wise statistics" << endl;
+		outfile << "************************" << endl;
+
+		outfile << endl << "Confusion_matrix count: Age-group-wise" << endl << endl;
+		for (int k = 0; k < 3; k++ )
+		{
+			outfile << get_age_group_enum_string( static_cast< age_groups >( k + 1 ) ) << "\t\t";
+			for ( int j = 0; j < 7; j++ )
+			{
+				outfile << get_expr_enum_string( static_cast< expressions >( j + 1 ) ) << '\t';
+				for ( int i = 0; i < 7; i++ )
+				{
+					outfile << '\t' << confusion_matrix_count_age_group_wise[ k ][ j ][i];
+				}
+				outfile << endl << "\t\t";
+			}
+			outfile << endl;
+		}
+		outfile << endl << "Confusion_matrix percentage: Age-group-wise" << endl << endl;
+		for ( int k = 0; k < 3; k++ )
+		{
+			outfile << get_age_group_enum_string( static_cast< age_groups >( k + 1 ) ) << "\t\t";
+	     	for ( int j = 0; j < 7; j++ )
+		   	{
+				outfile << get_expr_enum_string( static_cast< expressions >( j + 1 ) ) << '\t';
+				for ( int i = 0; i < 7; i++ )
+				{
+					outfile << '\t' << confusion_matrix_percentage_age_group_wise[ k ][ j ][i];
+				}
+				outfile << endl << "\t\t";
+		   	}
+			outfile << endl;
+		}
+		outfile << "Total number of images per age-group" << endl << endl;
+		for ( int k = 0; k < 3; k++ )
+		{
+			outfile << get_age_group_enum_string( static_cast< age_groups >( k + 1 ) ) << "\t\t" << age_group_count[ k ] << endl;
+		}
+		outfile << endl << "Total number of images correctly recognized per age-group" << endl << endl;
+		for ( int k = 0; k < 3; k++ )
+		{
+			outfile << get_age_group_enum_string( static_cast< age_groups >( k + 1 ) ) << "\t\t" << count_correct_recognition_age_group_wise[ k ] << endl;
+		}
+		outfile << endl << "Age-group-wise recognition rate" << endl << endl;
+		for ( int k = 0; k < 3; k++ )
+		{
+			outfile << get_age_group_enum_string( static_cast< age_groups >( k + 1 ) ) << "\t\t" << age_group_wise_recognition_rate[ k ] << endl;
+		}
+	}
+	outfile.close();
 }
 
 int main( int argc, char *argv[] )
@@ -176,8 +300,16 @@ int main( int argc, char *argv[] )
     	infile >> actual_expr;
     	infile >> recognized_expr;
 
-    	expr_actual_enum = get_enum_const( actual_expr );
-    	expr_recognized_enum = get_enum_const( recognized_expr );
+    	expr_actual_enum = get_expr_enum_const( actual_expr );
+    	expr_recognized_enum = get_expr_enum_const( recognized_expr );
+
+    	if ( FACES )
+    	{
+    		age_group_enum = find_age_group( imgname );
+    		confusion_matrix_count_age_group_wise[ age_group_enum - 1 ][ expr_actual_enum - 1 ][ expr_recognized_enum - 1 ]++;
+    		expression_count_age_group_wise[ age_group_enum - 1 ][ expr_actual_enum - 1 ]++;
+    		age_group_count[ age_group_enum - 1 ]++;
+    	}
 
     	confusion_matrix_count[ expr_actual_enum - 1 ][ expr_recognized_enum - 1 ]++;
     	expression_count[ expr_actual_enum - 1 ]++;
